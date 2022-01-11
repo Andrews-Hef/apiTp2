ffac9565011206fc35d346e92680054d6972e942
Andrews-Hef <andrews.agricole@gmail.com>
2022-01-11 11:38:31 +0100

    dd

--- src/Controller/ApiGenreController.php
+++ src/Controller/ApiGenreController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Genre;
 use App\Repository\GenreRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use PHPUnit\Util\Json;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +14,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\MakerBundle\Validator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ApiGenreController extends AbstractController
 {
@@ -46,21 +52,66 @@ class ApiGenreController extends AbstractController
     /**
      * @Route("/api/genres", name="api_genres_create", methods={"POST"})
      */
-    public function create(Request $request, SerializerInterface $serializer)
+    public function create(Request $request, EntityManagerInterface $manager,SerializerInterface $serializer, ValidatorInterface $validator)
     {
          $data= $request->getContent();
-         $genres =new Genre();
-         $serializer->deserialize($data,Genre::class,'')
-        $resultat = $serializer->serialize(
-            $genres, 
-            //2.15 video6
-        'json',
-        [
-            'groups' => ['listGenreSimple']
-        ] );
-        // return $this->render('api_genre/index.html.twig', [
-        //     'controller_name' => 'ApiGenreController',
-        // ]);
-        return new JsonResponse($resultat, 200,[], true);
+        // $genres =new Genre();
+        //$serializer->deserialize($data,Genre::class,'json',['object_to_populate'=> $genres]);
+        $genres= $serializer->deserialize($data,Genre::class,'json');
+
+        //gestion des erreurs de validation 
+        $errors =$validator->validate($genres);
+        if( count($errors)){
+            $errorsJson=$serializer->serialize($errors,'json');
+            return new JsonResponse($errorsJson,Response::HTTP_BAD_REQUEST,[], true);
+        }
+        $manager->persist($genres); 
+        $manager->flush(); 
+       
+        return new JsonResponse(
+            "le genre a bien été crée",
+            Response::HTTP_CREATED,
+            ["location"=> $this->generateUrl(
+                'api_genres_show',
+                ["id"=>$genres->getId()],
+                UrlGeneratorInterface::ABSOLUTE_URL)],
+                true);
     }
+    /**
+     * @Route("/api/genres/{id}", name="api_genres_update", methods={"PUT"})
+     */
+    public function edit(Genre $genres ,Request $request,EntityManagerInterface $manager, SerializerInterface $serializer, ValidatorInterface $validator)
+    {
+         
+        $data=$request->getContent();
+        $serializer->deserialize($data,Genre::class,'json',['object_to_populate'=>$genres]);
+        
+        //gestion des erreurs de validation 
+        $errors =$validator->validate($genres);
+        if( count($errors)){
+            $errorsJson=$serializer->serialize($errors,'json');
+            return new JsonResponse($errorsJson,Response::HTTP_BAD_REQUEST,[], true);
+        }
+
+        $manager->persist($genres); 
+        $manager->flush(); 
+        
+        return new JsonResponse("le genre a bien été modifié ", 200,[], true);
+      
+       
+       
+    }
+     /**
+     * @Route("/api/genres/{id}", name="api_genres_delete", methods={"DELETE"})
+     */
+    public function delete(Genre $genres ,EntityManagerInterface $manager)
+    {
+
+        $manager->remove($genres); 
+        $manager->flush(); 
+        
+        return new JsonResponse("le genre a bien été supprimé", 200,[], true);
+
+    }
+
 }
--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -2,14 +2,17 @@
 
 namespace App\Entity;
 
+use Symfony\Component\Validator\Constraints as Assert; 
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\GenreRepository;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass=GenreRepository::class)
+ * @UniqueEntity( fields={"libelle"}, message="le genre {{ value }} existe deja" )
  */
 class Genre
 {
@@ -24,6 +27,8 @@ class Genre
     /**
      * @ORM\Column(type="string", length=255)
      * @Groups({"listGenreSimple", "listGenreFull"})
+     * @Assert\Length(min=2,max=50,minMessage="le libelle doit contenir au moins {{ limit }} caratère", maxMessage="le libelle  doit contenir au plus {{ limit }} caratère")
+     * 
      */
     private $libelle;
 
